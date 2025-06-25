67f6dd97ba3499261efff68c5aa95c951acb1847
MaxPolsk <max_pol.90@mail.ru>
2025-06-25 11:25:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,29 @@
-const CACHE_NAME = "calc-ab-v1";
+const CACHE_NAME = 'calc-ab-v1';
 const urlsToCache = [
-  "/CalculatorAB/index.html",
-  "/CalculatorAB/styles.css",
-  "/CalculatorAB/script.js",
-  "/CalculatorAB/icons/icon-192x192.png",
-  "/CalculatorAB/icons/icon-512x512.png"
+  '/CalculatorAB/index.html',
+  '/CalculatorAB/',
+  '/CalculatorAB/логотип-5.png',
+  'https://maxpolsk.github.io/CalculatorAB/icons8-калькулятор-100.png'
 ];
 
-// Установка Service Worker
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
 });
 
-// Активация и очистка старых кэшей
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => 
       Promise.all(keys.map(key => 
         key !== CACHE_NAME ? caches.delete(key) : null
       ))
-    )
   );
 });
 
-// Обработка запросов
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => 
-      response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });

23227e92b5203f8024cd68994cb3e07aeb1d5639
MaxPolsk <max_pol.90@mail.ru>
2025-06-27 00:41:59 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calculator-v3';
+const CACHE_NAME = 'calculator-final-v1';
 const urlsToCache = [
   '/CalculatorAB/',
   '/CalculatorAB/index.html',
@@ -18,4 +18,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
 });
